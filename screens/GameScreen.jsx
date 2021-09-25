24962a8d55b5f458130d46a3ed24bc8eeeb9e6b0
Philip Kabranov <Kabranov12@gmail.com>
2021-09-25 12:08:23 -0700

    basic layout

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -20,11 +20,11 @@ export default function GameScreen() {
     if (timeLeft > 0) {
       // Time still left
       // TODO: update appropriate state variables
-      setTimeLeft(timeLeft - 10)
+      setTimeLeft(timeLeft - 10);
     } else {
       // Time has expired
       // TODO: update appropriate state variables
-      setRound(round + 1)
+      setRound(round + 1);
     }
   };
 
@@ -53,45 +53,65 @@ export default function GameScreen() {
 
   // Called when user taps a name option.
   // TODO: Update correct # and total # state values.
-  const selectedNameChoice = (index) => {
-  };
+  const selectedNameChoice = (index) => {};
 
   useEffect(() => {
     /* TODO: Call the countDown() method every 10 milliseconds */
+    // setInterval(countDown(), 10);
   }, [round]);
 
   // TODO: Finish this useEffect() hook such that we automatically
   // get the next round when the appropriate state variable changes.
 
-
   // Set up four name button components
   const nameButtons = [];
   for (let i = 0; i < 4; i++) {
     const j = i;
-    nameButtons.push(
+    nameButtons
+      .push
       // TODO: Implement a Button/Pressable type that shows a name choice, and implement the functionality when a user press on it
       // Hint: Most functionality is already taken care of by one of the functions already defined
-    );
+      ();
   }
 
   const timeRemainingStr = (timeLeft / 1000).toFixed(2);
 
   // Style & return the view.
   return (
-    <View style={{
-      justifyContent: "center",
-      alignItems: "center",
-      borderWidth: 5,
-      margin: 5,
-      padding: 5
-    }}>
-        <Image
-          source={require('../assets/MemberPictures/vaibhavgattani.jpg')}
-        />
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 5,
+        padding: 5,
+      }}
+    >
       {/* TODO: Build out your UI using Text and Image components. */}
       {/* Hint: What does the nameButtons list above hold? 
           What types of objects is this list storing?
           Try to get a sense of what's going on in the for loop above. */}
+      <Text style={styles.scoreText}>score</Text>
+      <Text style={styles.timerText}>timer</Text>
+      <Image
+        style={{
+          resizeMode: "cover",
+          height: 250,
+          width: 250,
+        }}
+        source={require("../assets/MemberPictures/willvavrik.jpg")}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Name 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Name 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Name 3</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Name 4</Text>
+      </TouchableOpacity>
     </View>
   );
 }
