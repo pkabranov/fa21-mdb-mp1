8d2b380734f19fffe287abda4dce781d2039d718
Philip Kabranov <Kabranov12@gmail.com>
2021-09-26 09:49:53 -0700

    features working

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -9,13 +9,16 @@ const names = Object.keys(nameToPic);
 
 export default function GameScreen() {
   // TODO: Declare and initialize state variables here, using "useState".
+  const [nameChoices, setNameChoices] = useState([]);
+  const [correctIndex, setCorrectIndex] = useState(0);
+  const [image, setImage] = useState("");
   const [correct, setCorrect] = useState(0);
   const [total, setTotal] = useState(0);
-  const [round, setRound] = useState(0);
+
   // State for the timer is handled for you.
   const [timeLeft, setTimeLeft] = useState(5000);
 
-  // Called by the timer every 10 seconds
+  // Called by the timer every 10 milliseconds
   const countDown = () => {
     if (timeLeft > 0) {
       // Time still left
@@ -24,7 +27,7 @@ export default function GameScreen() {
     } else {
       // Time has expired
       // TODO: update appropriate state variables
-      setRound(round + 1);
+      setTotal(total + 1);
     }
   };
 
@@ -48,70 +51,71 @@ export default function GameScreen() {
     nameOptions = shuffle(nameOptions);
 
     // TODO: Update state here.
+    setNameChoices(nameOptions);
+    setCorrectIndex(nameOptions.indexOf(correctName));
+    setImage(correctImage);
+
     setTimeLeft(5000);
   };
 
   // Called when user taps a name option.
   // TODO: Update correct # and total # state values.
-  const selectedNameChoice = (index) => {};
+  const selectedNameChoice = (index) => {
+    setTotal(total + 1);
+    if (index == correctIndex) {
+      setCorrect(correct + 1);
+    }
+  };
 
   useEffect(() => {
     /* TODO: Call the countDown() method every 10 milliseconds */
-    // setInterval(countDown(), 10);
-  }, [round]);
+    const timer = setInterval(() => {
+      countDown();
+    }, 10);
+    return () => clearInterval(timer);
+  });
 
   // TODO: Finish this useEffect() hook such that we automatically
   // get the next round when the appropriate state variable changes.
+
+  useEffect(() => {
+    getNextRound();
+  }, [total]);
 
   // Set up four name button components
   const nameButtons = [];
   for (let i = 0; i < 4; i++) {
     const j = i;
-    nameButtons
-      .push
+    nameButtons.push(
       // TODO: Implement a Button/Pressable type that shows a name choice, and implement the functionality when a user press on it
       // Hint: Most functionality is already taken care of by one of the functions already defined
-      ();
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => selectedNameChoice(j)}
+      >
+        <Text style={styles.buttonText}>{nameChoices[j]}</Text>
+      </TouchableOpacity>
+    );
   }
 
   const timeRemainingStr = (timeLeft / 1000).toFixed(2);
 
   // Style & return the view.
   return (
-    <View
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        margin: 5,
-        padding: 5,
-      }}
-    >
-      {/* TODO: Build out your UI using Text and Image components. */}
-      {/* Hint: What does the nameButtons list above hold? 
-          What types of objects is this list storing?
-          Try to get a sense of what's going on in the for loop above. */}
-      <Text style={styles.scoreText}>score</Text>
-      <Text style={styles.timerText}>timer</Text>
-      <Image
-        style={{
-          resizeMode: "cover",
-          height: 250,
-          width: 250,
-        }}
-        source={require("../assets/MemberPictures/willvavrik.jpg")}
-      />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Name 1</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Name 2</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Name 3</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Name 4</Text>
-      </TouchableOpacity>
+    <View syle={styles.imageView}>
+      <Text style={styles.scoreText}>
+        Current Score: {correct}/{total}
+      </Text>
+      <Text style={styles.timerText}>Time Remaining: {timeRemainingStr} s</Text>
+      <Image source={image} style={styles.image}></Image>
+      <View>
+        {
+          /* Hint: What does the nameButtons list above hold? 
+        What types of objects is this list storing?
+        Try to get a sense of what's going on in the for loop above. */
+          nameButtons
+        }
+      </View>
     </View>
   );
 }
